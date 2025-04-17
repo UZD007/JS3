d6cc8bdb993abd41b01e3f484b3b452b16f2f2cb
UZD007 <djdh9515@gmail.com>
2025-04-17 12:06:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let son=Math.floor(Math.random()*90+10);
+
+alert("Hozir sizda random son chiqishni boshlaydi 😊");
+alert("Random orqali chiqgan son: " +son);
+confirm("Biz bu sonni 2ga ko'paytirib bo'lmoqchimiz")
+
+alert(`Random orqali chiqgan son: ${son}\n ${son} ni ikkiga ko'paytirdik: ${son*2} \n ${son} ni ikkiga bo'ldik: ${son/2} \n ${son} ni uchga qoldiqli bo'ldik: ${son%3} `) 
